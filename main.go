767e9d8e4f6e418e69c0464c2b83172e2430854c
Dmitrij Tkacenko <dimot9@gmail.com>
2025-02-25 17:40:09 +0100

    add dynamic fitting of images to pages

--- main.go
+++ main.go
@@ -19,12 +19,32 @@ func main() {
 		pngFiles = append(pngFiles, "./images/"+file.Name())
 	}
 
-	pdf := gofpdf.New("P", "cm", "A4", "")
+	pdf := gofpdf.New("L", "cm", "A3", "")
+	pageWidth, pageHeight := pdf.GetPageSize()
 
 	for _, file := range pngFiles {
 		pdf.AddPage()
-		info := pdf.RegisterImage(file, "")
-		pdf.Image(file, 0, 0, info.Width(), info.Height(), false, "", 0, "")
+
+		pdf.RegisterImage(file, "")
+		// Get the dimensions of the image
+		imageInfo := pdf.GetImageInfo(file)
+		if imageInfo == nil {
+			log.Printf("Failed to get image info for %s", file)
+			continue
+		}
+		imgWidth, imgHeight := imageInfo.Width(), imageInfo.Height()
+
+		// Calculate the aspect ratio
+		aspectRatio := imgWidth / imgHeight
+		var newWidth, newHeight float64
+		if aspectRatio > pageWidth/pageHeight {
+			newWidth = pageWidth
+			newHeight = pageWidth / aspectRatio
+		} else {
+			newHeight = pageHeight
+			newWidth = pageHeight * aspectRatio
+		}
+		pdf.Image(file, 0, 0, newWidth, newHeight, false, "", 0, "")
 	}
 
 	// Save the PDF to a file
